c114e14e7e3fc9205e7bcb6018192128f1c0ca69
DhanyDelio <dhanydelio89@gmail.com>
2026-01-29 20:08:29 +0700

    feat: implement ProfileScreen UI and navigation setup

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:little_lemon_flutter/controller/profile_controller.dart';
 import 'package:little_lemon_flutter/utils/app_color.dart';
 import 'package:little_lemon_flutter/widget/profile_screen/information_header.dart';
+import 'package:little_lemon_flutter/widget/profile_screen/option.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -29,6 +31,33 @@ class _ProfileScreen extends State<ProfileScreen> {
           children: [
             InformationHeader(controller: controller),
             const SizedBox(height: 20),
+            Option(
+              icons: Icons.edit,
+              optionItem: () {
+                setState(() {
+                  debugPrint("context.push(Edit Screen)");
+                });
+              },
+              optionName: "Edit Data",
+            ),
+            const SizedBox(height: 5),
+            Option(
+              optionName: "about",
+              optionItem: () {
+                setState(() {});
+              },
+              icons: Icons.help,
+            ),
+            const SizedBox(height: 5),
+            Option(
+              optionName: "Log Out",
+              optionItem: () {
+                setState(() {
+                  context.go('/login');
+                });
+              },
+              icons: Icons.logout,
+            ),
           ],
         ),
       ),
--- lib/widget/profile_screen/option.dart
+++ lib/widget/profile_screen/option.dart
@@ -1,6 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:little_lemon_flutter/utils/app_color.dart';
 
-class Option extends StatelessWidget{
+class Option extends StatelessWidget {
   final String optionName;
-  final VoidCallback
+  final VoidCallback optionItem;
+  final IconData icons;
+
+  const Option({
+    super.key,
+    required this.optionName,
+    required this.optionItem,
+    required this.icons,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Material(
+          borderRadius: BorderRadius.horizontal(),
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: optionItem,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(icons, color: AppColor.primary, size: 24),
+                      const SizedBox(width: 20),
+                      Text(
+                        optionName,
+                        style: TextStyle(
+                          color: AppColor.primary,
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        const Divider(
+          thickness: 1,
+          height: 1,
+          indent: 20,
+          endIndent: 10,
+          color: AppColor.primary,
+        ),
+      ],
+    );
+  }
 }
